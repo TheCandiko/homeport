878954baa12868cce8ff8a03f3ef0ede24ba513f
TheCandiko <thecandiko@gmail.com>
2025-12-08 19:06:06 -0500

    Replace HomeportLogo with Header in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import HomeportLogo from '@/app/components/HomeportLogo';
+import Header from '@/app/components/Header';
 import WeeklyHighlight from '@/app/components/WeeklyHighlight';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-zinc-50 p-8 font-sans">
-      <header className="mx-auto flex w-full max-w-4xl justify-between">
-        <HomeportLogo />
-      </header>
+      <div className="mx-auto w-full max-w-4xl">
+        <Header />
+      </div>
       <main className="flex flex-1 items-center justify-center">
         <WeeklyHighlight />
       </main>
